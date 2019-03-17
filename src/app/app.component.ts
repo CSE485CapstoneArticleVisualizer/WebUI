06fa95a3a4416c7dc8652b15bf344b192e1e266d
Wenrong Deng <dwrandy520@gmail.com>
2019-03-17 16:06:19 -0700

    Minor change to filter box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'arvisui';
+export class AppComponent implements OnInit {
+
+  @ViewChild('sidenav') sidenav;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.sidenav.opened = true;
+  }
 }
