c573256a27f0188c8d482da3bd3092457d6a5864
Wenrong Deng <dwrandy520@gmail.com>
2019-04-07 12:11:13 -0700

    Temporary working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -48,9 +48,25 @@ export class HomeComponent implements OnInit {
       /** constructing the nodes array */
       for (let i = 0; i < data.articles.length; i++) {
         const n = new Node(data.articles[i]);
-        n.linkCount = data.articles.length - i;
+        // n.linkCount = data.articles.length - i;
         this.nodes.push(n);
       }
+
+      console.log(this.nodes);
+
+      for (let j = 0; j < data.links.length; j++) {
+        // console.log(data.links[j].source);
+        // console.log(data.links[j].target);
+
+        const sourceNode = this.nodes.find(n => n.id === data.links[j].source);
+        const targetNode = this.nodes.find(n => n.id === data.links[j].target);
+        sourceNode.linkCount++;
+        targetNode.linkCount++;
+        // console.log(sourceNode);
+        // console.log(targetNode);
+        // this.links.push(new Link(sourceNode, targetNode));
+      }
+
       this.links = data.links;
     });
   }
