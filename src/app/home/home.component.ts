0b9a9973fdb584a1104a0d3ed198929ee87ec8de
Adrian Kuo <akuo@macbook-pro-3.3520.dhcp.asu.edu>
2019-04-07 17:32:14 -0700

    Adjust filtering, update UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,59 +16,61 @@ export class HomeComponent implements OnInit {
   data: any;
 
   constructor(private http: HttpClient, private articleService: ArticleService) {
-    // const N = APP_CONFIG.N,
-    //   getIndex = number => number - 1;
+    const N = APP_CONFIG.N,
+      getIndex = number => number - 1;
 
-    // /** constructing the nodes array */
-    // for (let i = 1; i <= N; i++) {
-    //   this.nodes.push(new Node(i));
-    // }
+    /** constructing the nodes array */
+    for (let i = 1; i <= N; i++) {
+      this.nodes.push(new Node(i));
+    }
 
-    // for (let i = 1; i <= N; i++) {
-    //   for (let m = 2; i * m <= N; m++) {
-    //     /** increasing connections toll on connecting nodes */
-    //     this.nodes[getIndex(i)].linkCount++;
-    //     this.nodes[getIndex(i * m)].linkCount++;
+    for (let i = 1; i <= N; i++) {
+      for (let m = 2; i * m <= N; m++) {
+        /** increasing connections toll on connecting nodes */
+        this.nodes[getIndex(i)].linkCount++;
+        this.nodes[getIndex(i * m)].linkCount++;
 
-    //     /** connecting the nodes before starting the simulation */
-    //     this.links.push(new Link(i, i * m));
-    //   }
-    // }
+        /** connecting the nodes before starting the simulation */
+        this.links.push(new Link(i, i * m));
+      }
+    }
+
+    this.data = this.nodes;
   }
 
   ngOnInit() {
 
-    this.articleService.getArticlesByAuthor().subscribe(data => {
-      this.data = data;
-      APP_CONFIG.N = data.articles.length;
-      console.log(data.articles);
-      console.log(data.links);
-      // this.nodes = data.nodes;
-      this.links = data.links;
+    // this.articleService.getArticlesByAuthor().subscribe(data => {
+    //   this.data = data;
+    //   APP_CONFIG.N = data.articles.length;
+    //   console.log(data.articles);
+    //   console.log(data.links);
+    //   // this.nodes = data.nodes;
+    //   this.links = data.links;
 
-      /** constructing the nodes array */
-      for (let i = 0; i < data.articles.length; i++) {
-        const n = new Node(data.articles[i]);
-        // n.linkCount = data.articles.length - i;
-        this.nodes.push(n);
-      }
+    //   /** constructing the nodes array */
+    //   for (let i = 0; i < data.articles.length; i++) {
+    //     const n = new Node(data.articles[i]);
+    //     // n.linkCount = data.articles.length - i;
+    //     this.nodes.push(n);
+    //   }
 
-      console.log(this.nodes);
+    //   console.log(this.nodes);
 
-      for (let j = 0; j < data.links.length; j++) {
-        // console.log(data.links[j].source);
-        // console.log(data.links[j].target);
+    //   for (let j = 0; j < data.links.length; j++) {
+    //     // console.log(data.links[j].source);
+    //     // console.log(data.links[j].target);
 
-        const sourceNode = this.nodes.find(n => n.id === data.links[j].source);
-        const targetNode = this.nodes.find(n => n.id === data.links[j].target);
-        sourceNode.linkCount++;
-        targetNode.linkCount++;
-        // console.log(sourceNode);
-        // console.log(targetNode);
-        // this.links.push(new Link(sourceNode, targetNode));
-      }
+    //     const sourceNode = this.nodes.find(n => n.id === data.links[j].source);
+    //     const targetNode = this.nodes.find(n => n.id === data.links[j].target);
+    //     sourceNode.linkCount++;
+    //     targetNode.linkCount++;
+    //     // console.log(sourceNode);
+    //     // console.log(targetNode);
+    //     // this.links.push(new Link(sourceNode, targetNode));
+    //   }
 
-      this.links = data.links;
-    });
+    //   this.links = data.links;
+    // });
   }
 }
